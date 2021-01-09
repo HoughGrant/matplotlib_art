b500232aba4161e92c5690f1e1b48a02e6cbee8b
HoughGrant <houghgrant94@gmail.com>
2021-01-08 20:49:13 -0500

    mscatter is quicker

--- dots_and_pipes/dots_and_pipes.py
+++ dots_and_pipes/dots_and_pipes.py
@@ -2,7 +2,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def polka_dots(save_fig=False):
+def mscatter(x,y,ax=None, m=None, **kw):
+    import matplotlib.markers as mmarkers
+    if not ax: ax=plt.gca()
+    sc = ax.scatter(x,y,**kw)
+    if (m is not None) and (len(m)==len(x)):
+        paths = []
+        for marker in m:
+            if isinstance(marker, mmarkers.MarkerStyle):
+                marker_obj = marker
+            else:
+                marker_obj = mmarkers.MarkerStyle(marker)
+            path = marker_obj.get_path().transformed(
+                        marker_obj.get_transform())
+            paths.append(path)
+        sc.set_paths(paths)
+    return sc
+
+
+def polka_dots(save_fig=False, num_points=100):
     """
     I like the default color pallet here
     :param save_fig:
@@ -11,16 +29,18 @@ def polka_dots(save_fig=False):
     fig = plt.figure(frameon=True)
     ax = fig.add_axes([0, 0, 1, 1])
     ax.axis('off')
-    for i in range(100):
-        x, y = np.random.randint(0, 20, 2)
-        ax.scatter(x, y)
 
+    x_coords = np.random.randint(0, 20, num_points)
+    y_coords = np.random.randint(0, 20, num_points)
+    colors = [np.random.uniform(0, 1, 3) for x, y in zip(x_coords, y_coords)]
+
+    ax.scatter(x_coords, y_coords, c=colors)
     if save_fig:
         plt.savefig('polka_dots')
     plt.show()
 
 
-def vert_horz(save_fig=False):
+def vert_horz(save_fig=False, num_points: int = 1000):
     """
     I like the default color pallet here
     :param save_fig:
@@ -30,21 +50,20 @@ def vert_horz(save_fig=False):
     ax = fig.add_axes([0, 0, 1, 1])
     ax.set_facecolor((0.2, 0.25, 0.23))
     # ax.axis('off')
-    for i in range(1000):
-        x, y = np.random.randint(0, 100, 2)
-        if np.random.weibull(0.9) < x/100.:
-            marker = '|'
-        else:
-            marker = '_'
-        color_offset = np.random.random()
-        ax.scatter(x, y, marker=marker, c=[(0.7, 0.9, 0.03, color_offset * 0.9)])
+
+    x_coords = np.random.randint(0, 100, num_points)
+    y_coords = np.random.randint(0, 100, num_points)
+    markers = ['|' if np.random.weibull(0.9) < x/100. else '_' for x in x_coords]
+    colors = [(0.7, 0.9, 0.03, np.random.random() * 0.9) for x in x_coords]
+
+    mscatter(x_coords, y_coords, c=colors, m=markers, ax=ax)
 
     if save_fig:
         plt.savefig('vert_horz')
     plt.show()
 
 
-def vert_horz_wider(save_fig=False):
+def vert_horz_wider(save_fig=False, num_points=10000, marker_size=100):
     """
     I like the default color pallet here
     :param save_fig:
@@ -54,20 +73,20 @@ def vert_horz_wider(save_fig=False):
     ax = fig.add_axes([0, 0, 1, 1])
     ax.set_facecolor((0.2, 0.25, 0.23))
     # ax.axis('off')
-    for i in range(10000):
-        x, y = np.random.randint(0, 100, 2)
-        if np.random.weibull(0.9) < x/100.:
-            marker = '|'
-        else:
-            marker = '_'
-        color_offset = np.random.random()
-        ax.scatter(x, y, marker=marker, c=[(0.7, 0.9, 0.03, color_offset * 0.9)], s=100)
+
+    x_coords = np.random.randint(0, 100, num_points)
+    y_coords = np.random.randint(0, 100, num_points)
+    markers = ['|' if np.random.weibull(0.9) < x/100. else '_' for x in x_coords]
+    colors = [(0.7, 0.9, 0.03, np.random.random() * 0.9) for x in x_coords]
+
+    mscatter(x_coords, y_coords, c=colors, m=markers, ax=ax, s=marker_size)
 
     if save_fig:
         plt.savefig('vert_horz_pipez')
     plt.show()
 
 if __name__ == '__main__':
-    polka_dots()
-    # vert_horz()
-    # vert_horz_wider()
+    # polka_dots()
+    # vert_horz(num_points=1000)
+    vert_horz_wider()
+    # vert_horz_wider(marker_size=1000)
---
+++ empty_proj/empty_proj.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+
+def run_1():
+    pass
+
+
+if __name__ == '__main__':
+    run_1()
