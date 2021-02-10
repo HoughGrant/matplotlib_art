33962cedc76679a7c603f63069f60a88ec5fdf7f
HoughGrant <houghgrant94@gmail.com>
2021-02-10 18:52:58 -0500

    noisy mountains

--- pixel_swap/pixel_swap.py
+++ pixel_swap/pixel_swap.py
@@ -1,5 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+from scipy.ndimage import correlate
+from skimage.segmentation import random_walker
+from skimage.filters import gaussian
+from skimage.transform import rescale
 from skimage import io, color
 
 
@@ -37,5 +42,155 @@ def naive_approach():
     plt.show()
 
 
+def strange_noise():
+    image_rgb = np.random.uniform(0, 1, (100, 100, 3))
+    labels = np.random.randint(5, size=(100, 100))
+
+    for i in range(100):
+        segments = random_walker(image_rgb,
+                                 labels,
+                                 multichannel=True,
+                                 beta=150)
+
+        for color_index in np.unique(segments):
+            color = np.average(image_rgb * np.expand_dims(segments == color_index, axis=2), axis=(0, 1))
+            image_rgb[segments == color_index] = np.random.uniform(0, 1, 3)
+
+        if i % 10 == 0:
+            plt.figure()
+            plt.title(i)
+            plt.imshow(image_rgb)
+            plt.show()
+
+def background_generator():
+
+    size_x = 50
+    size_y = 50
+
+    n_labels = 2
+
+    image_rgb = np.random.uniform(0.9, 1, (size_x, size_y, 3))
+    labels = np.random.randint(n_labels + 1, size=(size_x, size_y)) * np.random.randint(0, 2, size=(size_x, size_y))
+
+    # segment random image
+    segments = random_walker(image_rgb,
+                             labels,
+                             multichannel=True,
+                             beta=250,
+                             copy=False,
+                             spacing=[50, 10])
+    for color_index in np.unique(segments):
+        image_rgb[segments == color_index] = np.random.uniform(0, 1, 3)
+
+    # transform segmented image so it is large, preserving blobs, and blurry
+    image_rgb = rescale(image_rgb, 2, anti_aliasing=False, multichannel=True)
+    image_rgb = gaussian(image_rgb, sigma=2, multichannel=True)
+    image_hsv = color.rgb2hsv(image_rgb)
+    buffer = 0.05
+    hsv_index = 1
+    for _ in range(10000):
+        rand_x, rand_y = np.random.choice(image_hsv.shape[0]), np.random.choice(image_hsv.shape[1])
+        orig_rgb = image_rgb[rand_x, rand_y]
+        rand_value = image_hsv[rand_x, rand_y, hsv_index]
+
+        x, y = np.where((rand_value * (1 + buffer) >= image_hsv[:, :, hsv_index]) & (
+                rand_value * (1 - buffer) < image_hsv[:, :, hsv_index]))
+        if len(x) == 0:
+            continue
+        idx = np.random.choice(len(x))
+        update_rgb = image_rgb[x[idx], y[idx]]
+
+        image_rgb[x[idx], y[idx]] = orig_rgb
+        image_rgb[rand_x, rand_y] = update_rgb
+
+    return image_rgb
+
+def noisy_mountains():
+
+    size_x = 50
+    size_y = 50
+
+    n_labels = 2
+
+    image_rgb = np.random.uniform(0.9, 1, (size_x, size_y, 3))
+    labels = np.random.randint(n_labels + 1, size=(size_x, size_y)) * np.random.randint(0, 2, size=(size_x, size_y))
+
+    # segment random image
+    segments = random_walker(image_rgb,
+                             labels,
+                             multichannel=True,
+                             beta=250,
+                             copy=False,
+                             spacing=[50, 10])
+    for color_index in np.unique(segments):
+        image_rgb[segments == color_index] = np.random.uniform(0, 1, 3)
+
+    # transform segmented image so it is large, preserving blobs, and blurry
+    image_rgb = rescale(image_rgb, 2, anti_aliasing=False, multichannel=True)
+    image_rgb = gaussian(image_rgb, sigma=2, multichannel=True)
+    image_hsv = color.rgb2hsv(image_rgb)
+    buffer = 0.05
+    hsv_index = 2
+
+    for pix_frac in [0.9]:
+        total_pixels_switched = int(image_rgb.shape[0] * image_rgb.shape[0] * pix_frac)
+        print(total_pixels_switched)
+        for _ in range(total_pixels_switched):
+            rand_x, rand_y = np.random.choice(image_hsv.shape[0]), np.random.choice(image_hsv.shape[1])
+            orig_rgb = image_rgb[rand_x, rand_y]
+            rand_value = image_hsv[rand_x, rand_y, hsv_index]
+
+            x, y = np.where((rand_value * (1 + 0.5 * buffer) >= image_hsv[:, :, hsv_index]) & (
+                    rand_value * (1 - 2 * buffer) < image_hsv[:, :, hsv_index]))
+            if len(x) == 0:
+                continue
+            idx = np.random.choice(len(x))
+            update_rgb = image_rgb[x[idx], y[idx]]
+
+            image_rgb[x[idx], y[idx]] = orig_rgb
+            image_rgb[rand_x, rand_y] = update_rgb
+
+        plt.figure()
+        plt.title(pix_frac)
+        plt.imshow(image_rgb)
+        plt.show()
+    plt.figure()
+    plt.imshow(image_rgb)
+    plt.show()
+
+    filter = np.array(1 * [(((1.0, 5.0, 1.0),
+                             (0.0, 0.0, 0.0),
+                             (-2.0, -5.0, -2.0)))])
+
+    # filter =  np.array(1*[(((-1.0, 0.0, 1.0),
+    #                         (-1.0, 0.0, 1.0),
+    #                         (-1.0, 0.0, 1.0)))])
+
+    filter /= np.sum(np.abs(filter))
+    filtered_img = correlate(image_rgb, filter)
+
+    filter_average = np.average(filtered_img)
+    filtered_img_binary = np.average(filtered_img[:], axis=2)
+    # filtered_img_binary[filtered_img_binary < 1.0 * filter_average] = 0
+    # filtered_img_binary[filtered_img_binary >= 1.0 * filter_average] = 1
+    plt.figure()
+    filtered_img_binary = (filtered_img_binary + np.abs(np.min(filtered_img_binary))) / np.max(
+        filtered_img_binary + np.abs(np.min(filtered_img_binary)))
+    # filtered_img_binary = 1 + filtered_img_binary
+    filtered_img_binary *= 2
+    plt.imshow(filtered_img_binary, cmap='gray')  # , vmin=0, vmax=1)
+    plt.colorbar()
+    plt.show()
+
+    hsv_index = 2
+    image_hsv = color.rgb2hsv(image_rgb)
+    image_hsv[:, :, hsv_index] *= filtered_img_binary
+    image_rgb_shadow_aug = color.hsv2rgb(image_hsv)
+    plt.figure()
+    plt.imshow(image_rgb_shadow_aug)
+    plt.show()
+
+
 if __name__ == '__main__':
-    naive_approach()
+    # naive_approach()
+    strange_noise()
