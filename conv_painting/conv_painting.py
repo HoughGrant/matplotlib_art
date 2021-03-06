6437ba60ffaee39e272f9046d7097ca0278e41f0
HoughGrant <houghgrant94@gmail.com>
2021-03-06 09:34:24 -0500

    agnes martin

--- conv_painting/conv_painting.py
+++ conv_painting/conv_painting.py
@@ -22,14 +22,15 @@ def run_1():
     plt.show()
 
 
-def run_2():
+def run_2(plot_points=False):
+    # matplotlib Agnes Martin
     fig, ax = plt.subplots()
     fig.patch.set_visible(False)
     ax = fig.add_axes([0, 0, 1, 1])
     ax.set_facecolor((0.6, 0.65, 0.67))
     line_nums = 10
 
-    x = np.linspace(0, 100, 1000)
+    x = np.linspace(0, 50, 1000)
 
     rand_color = randomcolor.RandomColor()
 
@@ -39,11 +40,15 @@ def run_2():
         offset = np.random.uniform(1.0, 2.0)
         y = np.sin(freq * (x - offset))
 
-        widths = range(1, 5)
+        widths = range(1, 50)
         cwt_output = cwt(y, ricker, widths)
         for output in cwt_output:
-            plt.plot(x, line_num * 1.0 + output, c=random_color[0])
+            if plot_points:
+                plt.scatter(x, line_num * 1.0 + output, color=random_color[0], s=0.1)
+            else:
+                plt.plot(x, line_num * 1.0 + output, c=random_color[0], linewidth=0.1)
     plt.show()
+
 
 if __name__ == '__main__':
     run_2()
