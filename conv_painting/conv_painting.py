a624087626f16055e9dac1383ab477bb7550ee71
HoughGrant <houghgrant94@gmail.com>
2021-03-07 07:51:02 -0500

    pleasant regions

--- conv_painting/conv_painting.py
+++ conv_painting/conv_painting.py
@@ -27,7 +27,7 @@ def run_2(plot_points=False):
     fig, ax = plt.subplots()
     fig.patch.set_visible(False)
     ax = fig.add_axes([0, 0, 1, 1])
-    ax.set_facecolor((0.6, 0.65, 0.67))
+    ax.set_facecolor((0.2, 0.23, 0.24))
     line_nums = 10
 
     x = np.linspace(0, 50, 1000)
@@ -44,11 +44,25 @@ def run_2(plot_points=False):
         cwt_output = cwt(y, ricker, widths)
         for output in cwt_output:
             if plot_points:
-                plt.scatter(x, line_num * 1.0 + output, color=random_color[0], s=0.1)
+                ax.scatter(x, line_num * 1.0 + output, color=random_color[0], s=0.3)
             else:
-                plt.plot(x, line_num * 1.0 + output, c=random_color[0], linewidth=0.1)
+                ax.plot(x, line_num * 1.0 + output, c=random_color[0], linewidth=0.1)
+
+    x_lims_current = ax.get_xlim()
+    y_lims_current = ax.get_ylim()
+
+    lower_xlim = np.random.uniform(*x_lims_current)
+    x_axes_length = np.random.uniform(2, 10)
+    upper_xlim = np.min([lower_xlim + x_axes_length, x_lims_current[1]])
+    ax.set_xlim(lower_xlim, upper_xlim)
+
+    lower_ylim = np.random.uniform(*y_lims_current)
+    y_axes_length = np.random.uniform(2, 10)
+    upper_ylim = np.min([lower_ylim + y_axes_length, y_lims_current[1]])
+    ax.set_ylim(lower_ylim, upper_ylim)
+
     plt.show()
 
 
 if __name__ == '__main__':
-    run_2()
+    run_2(True)
